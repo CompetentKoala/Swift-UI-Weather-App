805329c5ce5bb747b1f3c1edd5cfa11c4b9bb8b9
Cameron <cameronkuy@gmail.com>
2023-12-17 14:55:07 -0800

    Initial Commit

---
+++ Swiftui-Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swiftui-Weather
+//
+//  Created by Cameron Kuykendall on 12/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swiftui-Weather/Swiftui_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftui_WeatherApp.swift
+//  Swiftui-Weather
+//
+//  Created by Cameron Kuykendall on 12/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Swiftui_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
